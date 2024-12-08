26632d4ff655f9767750ad9ed81a8bdcb3ad88ab
Naghmana Imran <159182534+NaghmanaImran@users.noreply.github.com>
2024-12-08 21:49:10 +0500

    up

--- next.config.mjs
+++ next.config.mjs
@@ -4,14 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true, // Ensures compatibility with static exports
   },
-  async rewrites() {
-    return [
-      {
-        source: '/images/:path*',
-        destination: '/public/images/:path*',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
